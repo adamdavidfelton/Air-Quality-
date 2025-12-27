6ca405a2f9ff4531306f8d1ab9e197317753ef1b
Adam Felton <adam.d.felton@gmail.com>
2025-12-27 07:20:06 -0800

    new air quality project/testing git

---
+++ airquality.py
@@ -0,0 +1,8 @@
+#import modules
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#terminal setup
+pd.options.display.max_rows = 100
+pd.options.display.width = 0
